fd3aece920c602eeae551279a82689f443db5d44
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-25 16:48:47 -0300

    FOR

---
+++ caixaeletronico/src/FOR/EstruturaFor.java
@@ -0,0 +1,11 @@
+package FOR;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE","MARCOS","JOTARO","PUNPUN"};
+
+            for(int x=0;x<alunos.length;x++){
+                System.out.println("O aluno no indice x= "+ x + " é " + alunos[x] );
+            }
+    }
+}
