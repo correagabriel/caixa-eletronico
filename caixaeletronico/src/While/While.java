194f36719d069eb37dcd30065ff992b8ed672653
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-29 13:22:53 -0300

    while

---
+++ caixaeletronico/src/While/While.java
@@ -0,0 +1,25 @@
+package While;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.lang.model.util.Elements.Origin;
+
+public class While {
+    public static void main(String[] args) {
+        double mesada = 50.0D;
+
+            while(mesada>0){
+                Double valorDoce = valorAleatorio();
+                    if(valorDoce > mesada)
+                        valorDoce =  mesada;
+                        System.out.println("Doce do valor : "+ valorDoce + "Adicionado no carrinho");
+                            mesada = mesada - valorDoce;
+            }
+
+                System.out.println("Mesada : " + mesada);
+                System.out.println("Joaozinho gastou toda a sua mesada em doces ");
+    }
+
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble( 2,  8);
+    }
+}
